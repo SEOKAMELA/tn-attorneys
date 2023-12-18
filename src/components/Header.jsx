7706f8b768a3523361fddac1fa7e3b2996956a27
Seokamela <ntjobokoane@gmail.com>
2023-12-18 15:36:30 +0200

    large fils

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,38 @@
 // Header.jsx
-import React from 'react';
-import './Header.css';
-import image from '../assets/images.png';
+import React from "react";
+import "./Header.css";
+import image from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div className='header-div'>
-      <div className='logo-name-div'>
-        <img src={image} alt='Logo' className='logo-img' />
-        <h1 className='logo-name'>Tn Attornys</h1>
+    <div className="header-div">
+      <div className="logo-name-div">
+        <img src={image} alt="Logo" className="logo-img" />
+        <h2 className="logo-name">TN Attornys</h2>
       </div>
-      <div className='nav-bar-div'>
+      <div className="nav-bar-div">
         <nav>
           <ul>
-            <li><a href='/#'>Home</a></li>
-            <li><a href='/#'>About</a></li>
-            <li><a href='/#'>Services</a></li>
-            <li><a href='/#'>Contact</a></li>
+            <li>
+              <a href="/#">Home</a>
+            </li>
+            <li>
+              <a href="/#">About Us</a>
+            </li>
+            <li>
+              <a href="/#">Our Services</a>
+            </li>
+            <li>
+              <a href="/#">Contact Us</a>
+            </li>
           </ul>
         </nav>
       </div>
-      <div className='contacts-div'>
-        <p>Phone: (123) 456-7890</p>
+      <div className="contacts-div">
+      <i class="fa-solid fa-phone"></i> (+27) 10 00650 290 <p></p>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
